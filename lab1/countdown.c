2d9b13bc641321beb9a7c91f43419d0ff96654e9
TommyFurgi <tomasz@DESKTOP-4UV0RR1>
2024-03-11 11:49:27 +0100

    lab1

---
+++ lab1/countdown.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "function.h"
+
+int main() {
+    int i;
+    for (i = 10; i >= 0; i--) {
+        printf("%d\n", i);
+    }
+    
+    printf("%d \n", function());
+    return 0;
+}
