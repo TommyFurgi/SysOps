cfd50517651ecc1a626d33aef71a4ab4c34ab6ae
TommyFurgi <tomasz@DESKTOP-4UV0RR1>
2024-03-11 11:50:59 +0100

    lab2, libraries

---
+++ lab2/collatz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "collatz.h"
+
+int collatz_conjecture(int value) {
+    if ((value % 2) == 0) 
+        return (value/2);
+    else 
+        return (3*value + 1);
+} 
+
+int test_collatz_convergence(int value, int max_iter) {
+    int counter = 0;
+
+    if (value == 1)
+        return 0;
+
+    while (counter < max_iter) {
+        value = collatz_conjecture(value);
+        counter += 1;
+
+        if (value == 1)
+            return counter;
+    }   
+    return -1;
+}
